5881150a429bfb5ac4cad1ea5636651a270742e8
Frode Roxrud Gill <extfrg@brreg.no>
2019-05-13 15:28:53 +0200

    Added transformation manager

---
+++ src/test/java/no/nsg/controller/TransformationTest.java
@@ -0,0 +1,30 @@
+package no.nsg.controller;
+
+import net.sf.saxon.s9api.*;
+import no.nsg.repository.TransformationManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.ByteArrayOutputStream;
+import java.io.UnsupportedEncodingException;
+
+
+@RunWith(SpringRunner.class)
+public class TransformationTest {
+
+    TransformationManager transformationManager = new TransformationManager();
+
+    @Test
+    public void finvoiceTransformTest() throws SaxonApiException, UnsupportedEncodingException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+        transformationManager.transform(getClass().getClassLoader().getResourceAsStream("openAPI/examples/Finvoice.xml"),
+                                        TransformationManager.FINVOICE_TO_XBRL,
+                                        baos);
+
+        baos.toString("UTF-8");
+    }
+
+}
