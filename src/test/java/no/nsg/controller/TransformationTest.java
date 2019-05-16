42a3564268de1605b16a070fa5f89144fc3e2244
Frode Roxrud Gill <extfrg@brreg.no>
2019-05-16 09:34:20 +0200

    More finvoice examples

--- src/test/java/no/nsg/controller/TransformationTest.java
+++ src/test/java/no/nsg/controller/TransformationTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 
 
@@ -15,15 +16,23 @@ public class TransformationTest {
 
     TransformationManager transformationManager = new TransformationManager();
 
+    private InputStream getResourceAsStream(final String resource) {
+        return getClass().getClassLoader().getResourceAsStream(resource);
+    }
+
     @Test
-    public void finvoiceTransformTest() throws SaxonApiException, UnsupportedEncodingException {
+    public void finvoiceHappydayTransformTest() throws SaxonApiException, UnsupportedEncodingException {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-        transformationManager.transform(getClass().getClassLoader().getResourceAsStream("openAPI/examples/Finvoice.xml"),
-                                        TransformationManager.FINVOICE_TO_XBRL,
-                                        baos);
-
+        transformationManager.transform(getResourceAsStream("finvoice/Finvoice.xml"), TransformationManager.FINVOICE_TO_XBRL, baos);
         baos.toString("UTF-8");
     }
 
+    @Test
+    public void finvoiceTransformTest() throws SaxonApiException, UnsupportedEncodingException {
+        transformationManager.transform(getResourceAsStream("finvoice/Finvoice.xml"), TransformationManager.FINVOICE_TO_XBRL, new ByteArrayOutputStream());
+        transformationManager.transform(getResourceAsStream("finvoice/finvoice 75 myynti.xml"), TransformationManager.FINVOICE_TO_XBRL, new ByteArrayOutputStream());
+        transformationManager.transform(getResourceAsStream("finvoice/finvoice 76 myynti.xml"), TransformationManager.FINVOICE_TO_XBRL, new ByteArrayOutputStream());
+        transformationManager.transform(getResourceAsStream("finvoice/finvoice 77 myynti.xml"), TransformationManager.FINVOICE_TO_XBRL, new ByteArrayOutputStream());
+        transformationManager.transform(getResourceAsStream("finvoice/finvoice 78 myynti.xml"), TransformationManager.FINVOICE_TO_XBRL, new ByteArrayOutputStream());
+    }
 }
