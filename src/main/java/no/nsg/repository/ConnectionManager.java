fdaab8eb1c7671d0afd6c9368d87f29b2ad938e7
Frode Roxrud Gill <extfrg@brreg.no>
2019-06-21 10:57:54 +0200

    Log connection parameters

--- src/main/java/no/nsg/repository/ConnectionManager.java
+++ src/main/java/no/nsg/repository/ConnectionManager.java
@@ -57,7 +57,9 @@ public class ConnectionManager {
 			}
 
 			if (requireDboPermissions) { //This happens only at application startup. Do some extra logging
-				LOGGER.info("Connecting to " + postgresDbUrl);
+				LOGGER.info("postgres.nsg.db_url  : " + postgresDbUrl);
+				LOGGER.info("postgres.nsg.dbo_user: " + postgresDboUser);
+				LOGGER.info("postgres.nsg.user    : " + postgresUser);
 			}
 
 			Connection connection = DriverManager.getConnection(postgresDbUrl, username, password);
