e55bd57f4d81bb8b66d01f9e2973fe128e2e72d6
Bjørn Grøva <bjorn.grova@brreg.no>
2019-08-29 09:19:02 +0200

    force build

--- src/main/java/no/nsg/controller/InvoicesApiControllerImpl.java
+++ src/main/java/no/nsg/controller/InvoicesApiControllerImpl.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-//force build 3434343
+//force build 3434343 675747
 
 @Controller
 public class InvoicesApiControllerImpl implements no.nsg.generated.invoice_api.InvoicesApi {
