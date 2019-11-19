2eb3fe18915c88e0c5723961788745f224dc4e9e
Frode Roxrud Gill <extfrg@brreg.no>
2019-11-19 12:57:52 +0100

    Removed unused code

--- src/main/java/no/nsg/Application.java
+++ src/main/java/no/nsg/Application.java
@@ -13,7 +13,6 @@ import no.nsg.spring.CachableDispatcherServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ExitCodeExceptionMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
