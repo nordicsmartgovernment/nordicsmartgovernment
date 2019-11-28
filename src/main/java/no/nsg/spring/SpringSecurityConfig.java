98bd0b93b7ac2cfc6a84d19ce90ca7d0bc969b14
Frode Roxrud Gill <extfrg@brreg.no>
2019-11-28 16:43:51 +0100

    Remove authentication (it will be replaced by a REST path param)

--- src/main/java/no/nsg/spring/SpringSecurityConfig.java
+++ src/main/java/no/nsg/spring/SpringSecurityConfig.java
@@ -1,46 +1,20 @@
 package no.nsg.spring;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    PasswordEncoder passwordEncoding() {
-        return new NullPasswordEncoder();
-    }
-
-    @Autowired
-    private CustomUserDetailsService customUserDetailsService;
-
-    @Autowired
-    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-        auth.eraseCredentials(false)
-            .userDetailsService(customUserDetailsService)
-            .passwordEncoder(passwordEncoding());
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeRequests()
-                .antMatchers(HttpMethod.OPTIONS).permitAll()
-                .antMatchers("/ping").permitAll()
-                .antMatchers("/ready").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .userDetailsService(customUserDetailsService)
-                .httpBasic();
+                .anyRequest().permitAll();
     }
 
 }
