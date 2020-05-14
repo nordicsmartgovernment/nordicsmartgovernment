e280f2e8a90df8ffd364b8353efd99ca6ff99a1c
Frode Roxrud Gill <extfrg@brreg.no>
2020-05-14 15:07:11 +0200

    Allow anyone to call us

--- src/main/java/no/nsg/spring/SpringSecurityConfig.java
+++ src/main/java/no/nsg/spring/SpringSecurityConfig.java
@@ -13,6 +13,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
+            .cors().disable()
             .authorizeRequests()
                 .anyRequest().permitAll();
     }
