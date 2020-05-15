26eaad4b7cf897e6992461f2f3ea1ff80d1a41a6
Frode Roxrud Gill <extfrg@brreg.no>
2020-05-15 13:59:52 +0200

    Explicitly setting cors "*"

--- src/main/java/no/nsg/controller/TransactionsApi.java
+++ src/main/java/no/nsg/controller/TransactionsApi.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.xml.sax.SAXException;
 
@@ -24,6 +25,7 @@ import java.util.NoSuchElementException;
 
 @Controller
 @RestControllerAdvice
+@CrossOrigin
 public class TransactionsApi implements no.nsg.generated.transaction_api.TransactionsApi {
     private static final Logger LOGGER = LoggerFactory.getLogger(TransactionsApi.class);
 
