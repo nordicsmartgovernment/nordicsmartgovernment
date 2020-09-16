34d3855ef4af1e01838fc2c0ac25439837654881
hconhegg <heggholmen3@gmail.com>
2020-09-16 14:45:40 +0200

    GuessFormat refactoring reverted. Crisis averted. The wrong types were being returned. No longer returns OTHER on failed guesses where the document indicates there should be a type.

--- src/main/java/no/nsg/repository/document/FormatFactory.java
+++ src/main/java/no/nsg/repository/document/FormatFactory.java
@@ -35,31 +35,38 @@ public class FormatFactory {
     public static DocumentFormat.Format guessFormat(final DocumentType.Type documentType, final String document) {
         if (document.contains("<Finvoice ")) {
             if (document.contains("<InvoiceTypeCode>REC")) {
-                return mapToPurchaseOrSale(documentType, FINVOICE_SALES_RECEIPT, FINVOICE_PURCHASE_RECEIPT);
+                if (DocumentType.Type.SALES_RECEIPT == documentType) {
+                    return FINVOICE_SALES_RECEIPT;
+                } else if (DocumentType.Type.PURCHASE_RECEIPT == documentType) {
+                    return FINVOICE_PURCHASE_RECEIPT;
+                }
             } else {
-                return mapToPurchaseOrSale(documentType, FINVOICE_SALES_INVOICE, FINVOICE_PURCHASE_INVOICE);
+                if (DocumentType.Type.SALES_INVOICE == documentType) {
+                    return FINVOICE_SALES_INVOICE;
+                } else if (DocumentType.Type.PURCHASE_INVOICE == documentType) {
+                    return FINVOICE_PURCHASE_INVOICE;
+                }
             }
         } else if (document.contains("xmlns=\"urn:iso:std:iso:20022:tech:xsd:camt.053.")) {
             return DocumentFormat.Format.CAMT_053_001_08;
         } else if (document.contains("xmlns=\"urn:oasis:names:specification:ubl:schema:xsd:Invoice-2\"") || document.contains("<Invoice ")) {
-            return mapToPurchaseOrSale(documentType, UBL_2_1_SALES_INVOICE, UBL_2_1_PURCHASE_INVOICE);
+            if (DocumentType.Type.SALES_INVOICE == documentType) {
+                return DocumentFormat.Format.UBL_2_1_SALES_INVOICE;
+            } else if (DocumentType.Type.PURCHASE_INVOICE == documentType) {
+                return DocumentFormat.Format.UBL_2_1_PURCHASE_INVOICE;
+            }
         } else if (document.contains("xmlns=\"urn:oasis:names:specification:ubl:schema:xsd:Order-2\"") || document.contains("<Order ")) {
-            return mapToPurchaseOrSale(documentType, UBL_2_1_SALES_ORDER, UBL_2_1_PURCHASE_ORDER);
+            if (DocumentType.Type.SALES_ORDER == documentType) {
+                return DocumentFormat.Format.UBL_2_1_SALES_ORDER;
+            } else if (DocumentType.Type.PURCHASE_ORDER == documentType) {
+                return DocumentFormat.Format.UBL_2_1_PURCHASE_ORDER;
+            }
         } else if (document.contains("<xbrli:xbrl")) {
             return DocumentFormat.Format.XBRL_GL;
         } else {
             return DocumentFormat.Format.OTHER;
         }
-
-    }
-
-    private static DocumentFormat.Format mapToPurchaseOrSale(DocumentType.Type documentType, DocumentFormat.Format salesFormat, DocumentFormat.Format purchaseFormat) {
-        if (DocumentType.Type.SALES_RECEIPT == documentType) {
-            return salesFormat;
-        } else if (DocumentType.Type.PURCHASE_RECEIPT == documentType) {
-            return purchaseFormat;
-        }
-        return OTHER;
+        throw new IllegalArgumentException("Could not find document format from type" + documentType.name());
     }
 
     public static boolean isCompatible(final DocumentType.Type documentType, final DocumentFormat.Format documentFormat) {
