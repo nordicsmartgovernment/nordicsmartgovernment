f0b48efb3d6cc50a112865d5c508ed0df6cdfd83
extfrg <extfrg@brreg.no>
2021-10-08 14:07:55 +0200

    More ignore experimenting

--- src/main/java/no/nsg/spring/ExcludeFromJacocoGeneratedReport.java
+++ src/main/java/no/nsg/spring/ExcludeFromJacocoGeneratedReport.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.FIELD, ElementType.TYPE})
 public @interface ExcludeFromJacocoGeneratedReport {
 }
